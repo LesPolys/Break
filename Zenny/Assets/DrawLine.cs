74fea21de6bc05a98e3a2d25005ae6bcc57352ad
Logan Stewart <stewart.jlt@gmail.com>
2018-06-26 22:51:42 -0400

    TestPush

---
+++ Zenny/Assets/DrawLine.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+
+	public float width;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+		//DrawLine (transform.position, new Vector3 (0.0f, 0.0f, 0.0f), Color.white, width);
+	}
+
+}
---
+++ Zenny/Assets/MouseFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour {
+
+
+	LineRenderer lr;
+
+	public RectTransform stage1;
+	public RectTransform stage2;
+	public float speed;
+
+
+	void Awake()
+	{
+		lr = GetComponent<LineRenderer>();
+	}
+
+	void Start()
+	{
+		lr.SetVertexCount(2);
+		lr.SetWidth(0.3f, 0.3f);
+		StartCoroutine(AnimateLineBetween(stage1, stage2));
+	}
+
+
+	IEnumerator AnimateLineBetween(RectTransform a, RectTransform b)
+	{
+		// set first point
+		lr.SetPosition(0, a.anchoredPosition3D);
+		// initialize second point
+		lr.SetPosition(1, a.anchoredPosition3D);
+
+		// the distance (and direction) between the two points
+		Vector3 distance = b.anchoredPosition3D - a.anchoredPosition3D;
+		for (float i = 0; i < 1; i += speed / 200)
+		{
+			// each frame, advance a fraction of the way
+			lr.SetPosition(1, distance * i);
+			yield return null;
+		}
+	}
+}
