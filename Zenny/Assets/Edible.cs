86e8365c7fd15b32880afb2b075547b614b3f174
stangmar <stewart.jlt@gmail.com>
2018-06-27 16:28:54 -0400

    TouchInput
    feel the difference

---
+++ Zenny/Assets/Edible.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edible : MonoBehaviour {
+
+	public Material[] mats;
+	Renderer theRenderer;
+	void Awake(){
+		theRenderer = GetComponent<Renderer> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setMat(int i){
+		theRenderer.material = mats [i];
+	}
+
+	public void OnTriggerEnter(Collider collider){
+		if(collider.gameObject.tag == "Tadpole"){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Zenny/Assets/Scripts/DotSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotSpawner : MonoBehaviour {
+
+	public GameObject dotPrefab;
+	public float timeBetweenDots;
+
+	public float[] possibleRadius;
+
+	void Awake(){
+		StartCoroutine (SpawnDot());
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnDot(){
+		while (true) {
+
+			yield return new WaitForSeconds(timeBetweenDots);
+			GameObject temp = Instantiate (dotPrefab);
+			int tempIndex = Random.Range (0, possibleRadius.Length);
+			temp.transform.position = Random.insideUnitCircle.normalized * possibleRadius[tempIndex];
+			temp.GetComponent<Edible> ().setMat (tempIndex);
+		
+		}
+		yield return null;
+	}
+
+
+}
